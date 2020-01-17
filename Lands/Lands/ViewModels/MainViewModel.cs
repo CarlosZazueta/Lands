94dac6a7b2d18dd3d88c9002b2afd9db01c369ec
Carlos Clemente Zazueta Glez <carlosglez@MacBook-Air-de-Clemente.local>
2020-01-17 09:53:16 -0700

    Agregados bindings a LoginPage

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
         public MainViewModel()
         {
+            this.Login = new LoginViewModel();
         }
+        #endregion
     }
 }
