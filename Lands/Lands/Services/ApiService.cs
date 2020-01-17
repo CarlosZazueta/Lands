3db3255f618af7201a9506b98b899f8fd987aa2e
Carlos Clemente Zazueta Glez <carlosglez@MacBook-Air-de-Clemente.local>
2020-01-17 14:34:28 -0700

    ApiServices añadido

---
+++ Lands/Lands/Services/ApiService.cs
@@ -0,0 +1,66 @@
+﻿namespace Lands.Services
+{
+    using Models;
+    using Newtonsoft.Json;
+    using Plugin.Connectivity;
+    using System;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ApiService
+    {
+        public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings."
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Check your internet connection."
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+                Message = "Ok"
+            };
+        }
+
+        public async Task<TokenResponse> GetToken(
+            string urlBase,
+            string username,
+            string password)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var response = await client.PostAsync("Token",
+                    new StringContent(string.Format(
+                        "grant_type=password&username={0}&password={1}",
+                        username, password),
+                        Encoding.UTF8, "application/x-www-form-urlencoded"));
+                var resultJSON = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<TokenResponse>(
+                    resultJSON);
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
